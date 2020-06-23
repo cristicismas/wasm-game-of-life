964627873de9b36346fb82919ae70f059613e6b0
Cristi Cismas <cristicismas99@gmail.com>
2020-06-23 12:35:57 +0300

    Update the game of live to return cell pointers instead of unicode

--- src/lib.rs
+++ src/lib.rs
@@ -64,10 +64,7 @@ impl Universe {
         self.cells = next;
     }
 
-    pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
-
+    pub fn new(width: u32, height: u32) -> Universe {
         let cells = (0..width * height)
             .map(|i| {
                 if i % 2 == 0 || i % 7 == 0 {
@@ -83,6 +80,18 @@ impl Universe {
             height,
             cells,
         }
+    }
+    
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 
     pub fn render(&self) -> String {
